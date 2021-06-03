bfab77af770e7fb92704f314e577c552c6e8237f
ShifterBit <48891590+ShifterBit@users.noreply.github.com>
2021-06-03 07:03:42 +0000

    fix indentation on git global ignore

--- users/profiles/git/default.nix
+++ users/profiles/git/default.nix
@@ -6,44 +6,44 @@
       pull.rebase = false;
     };
     ignores = [
-    # swap
-    "[._]*.s[a-v][a-z]"
-    "[._]*.sw[a-p]"
-    "[._]s[a-v][a-z]"
-    "[._]sw[a-p]"
-    # session
-    "Session.vim"
-    # temporary
-    ".netrwhist"
-    "*~"
-    # projectiles files
-    ".projectile"
+      # swap
+      "[._]*.s[a-v][a-z]"
+      "[._]*.sw[a-p]"
+      "[._]s[a-v][a-z]"
+      "[._]sw[a-p]"
+      # session
+      "Session.vim"
+      # temporary
+      ".netrwhist"
+      "*~"
+      # projectiles files
+      ".projectile"
 
-    # directory configuration
-    ".dir-locals.el"
-    "*~"
-    "\#*\#"
-    "/.emacs.desktop"
-    "/.emacs.desktop.lock"
-    "*.elc"
-    "auto-save-list"
-    "tramp"
-    ".\#*"
-    # Logs and databases #
-    ######################
-    "*.log"
-    "*.sql"
-    "*.sqlite"
+      # directory configuration
+      ".dir-locals.el"
+      "*~"
+      "\#*\#"
+      "/.emacs.desktop"
+      "/.emacs.desktop.lock"
+      "*.elc"
+      "auto-save-list"
+      "tramp"
+      ".\#*"
+      # Logs and databases #
+      ######################
+      "*.log"
+      "*.sql"
+      "*.sqlite"
 
-    # OS generated files #
-    ######################
-    ".DS_Store"
-    ".DS_Store?"
-    "._*"
-    ".Spotlight-V100"
-    ".Trashes"
-    "ehthumbs.db"
-    "Thumbs.db"
+      # OS generated files #
+      ######################
+      ".DS_Store"
+      ".DS_Store?"
+      "._*"
+      ".Spotlight-V100"
+      ".Trashes"
+      "ehthumbs.db"
+      "Thumbs.db"
     ];
   };
 }
