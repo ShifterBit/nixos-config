7224cf324f1abced27419d8ba39ecbeb019264eb
ShifterBit <48891590+ShifterBit@users.noreply.github.com>
2021-07-01 10:57:32 -0400

    update xfce config

--- profiles/graphical/xfce/default.nix
+++ profiles/graphical/xfce/default.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }: {
-  services.xserver.windowManager.xfce = {
+  services.xserver.desktopManager.xfce = {
     enable = true;
     thunarPlugins = with pkgs; [ xfce.thunar-volman xfce.thunar-archive-plugin ];
   };
+  environment.systemPackages = with pkgs; [
+    plank
+    picom
+    xfce.xfce4-notifyd
+    xfce.xfce4-whiskermenu-plugin
+    xfce.xfce4-power-manager
+  ];
 }
